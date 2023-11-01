dbdf402a3a9cb1412d23f64bd37ef84ba83319ae
Maher Tubasi <114957621+tubasi18@users.noreply.github.com>
2023-11-01 22:21:40 +0200

    Delete assignment1

--- assignment1/src/assignment1/Interfaces/IReportsGenerator.java
+++
@@ -1,8 +0,0 @@
-package assignment1.Interfaces;
-import java.util.List;
-
-public interface IReportsGenerator {
-    void generateAvailableCoursesReport(List<ICourse> courses);
-    void generateStudentCoursesReport(IStudent student);
-    void generateCourseStudentsReport(ICourse course) ;
-}
